d742b83145d31962510e3ee046df5acd922efa84
Robert Rayborn <rrayborn@mozilla.com>
2015-02-10 23:22:14 -0800

    Add deployment script.

---
+++ utilities/scripts/deploy.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Check out /home/shared/code
+cd /home/shared/code
+git pull
+chgrp -f     -R advocacy ..
+chmod 771 -f -R          ..
+
+# Check out /var/server/server
+cd /var/server/
+cp -r /home/shared/code/flask/* server
+chgrp -f     -R advocacy        server
+chmod 775 -f -R                 server
