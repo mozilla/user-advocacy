77546ec1e33a82f136f6dbea31155862c1150599
Robert Rayborn <rrayborn@mozilla.com>
2015-02-13 15:31:39 -0800

    Add cron and user check to deploy.sh

--- utilities/scripts/deploy.sh
+++ utilities/scripts/deploy.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+
+# Make sure user is root
+if ( (( EUID != 0 )) || [ ! "$BASH_VERSION" ] ); then
+    echo "Please run as root and use bash not sh to invoke." 1>&2
+    exit 1
+fi
+
+python $CODE_PATH/pipelines/cron/scheduler.py
+
 # Check out /home/shared/code
 cd /home/shared/code
 git pull
@@ -11,4 +20,5 @@ cp -r /home/shared/code/flask/* server
 chgrp -f     -R advocacy        server
 chmod 775 -f -R                 server
 
+# restart server
 service apache2 restart
