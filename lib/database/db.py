68628f7c33a0c22724f127c279f1012cbbebdf52
Cheng Wang <cww@mozilla.com>
2015-02-13 22:04:03 -0800

    Force DB engines to use unicode objects for string columns

--- lib/database/db.py
+++ lib/database/db.py
@@ -14,7 +14,7 @@ logging.getLogger('sqlalchemy.engine').setLevel(logging.INFO)
 
 
 def init_engine (uri):
-    return sqlalchemy.create_engine(uri, echo=False, encoding='utf-8')
+    return sqlalchemy.create_engine(uri, echo=False, encoding='utf-8', use_unicode=True)
 
 class Database(object):
     """Database object. Represents an explicitly named DB accessed from a given engine."""
