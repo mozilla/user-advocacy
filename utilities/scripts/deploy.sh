d034da926e3700dbfab81b83d2dde6dfa5e75a79
Cheng Wang <cww@mozilla.com>
2015-02-13 23:36:21 -0800

    Make deploy.sh use the right python by setting the environment first

--- utilities/scripts/deploy.sh
+++ utilities/scripts/deploy.sh
@@ -6,19 +6,23 @@ if ( (( EUID != 0 )) || [ ! "$BASH_VERSION" ] ); then
     exit 1
 fi
 
+workon uabackend 
+
 python $CODE_PATH/pipelines/cron/scheduler.py
 
-# Check out /home/shared/code
-cd /home/shared/code
+# Check out $CODE_PATH
+cd $CODE_PATH
 git pull
 chgrp -f     -R advocacy ..
 chmod 771 -f -R          ..
 
 # Check out /var/server/server
 cd /var/server/
-cp -r /home/shared/code/flask/* server
+cp -r $CODE_PATH/flask/* server
 chgrp -f     -R advocacy        server
 chmod 775 -f -R                 server
+
+deactivate
 
 # restart server
 service apache2 restart
