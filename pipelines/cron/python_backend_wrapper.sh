f0e74215d7511853965dd9a9460920042e82d861
Robert Rayborn <rrayborn@mozilla.com>
2015-02-19 15:11:06 -0800

    Commentted and improved the scheduler

---
+++ pipelines/cron/python_backend_wrapper.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#Gets us in to our virtualenv and runs python
+
+echo $HOME
+export WORKON_HOME=$HOME/.virtualenvs
+echo $WORKON_HOME
+export PROJECT_HOME=$HOME/
+echo $PROJECT_HOME
+source $WORKON_HOME/uabackend/bin/virtualenvwrapper.sh
+
+workon uabackend
+cd $CODE_PATH
+
+echo "CODE_PATH IS $CODE_PATH"
+echo "PATH IS $(pwd)"
+
+python $1
+
+deactivate
--- pipelines/cron/scheduler.py
+++ pipelines/cron/scheduler.py
@@ -1,7 +1,22 @@
 #!/usr/local/bin/python
+
+'''
+Manages our crontabs.
+'''
+#TODO(rrayborn): figure out why it's adding whitespace to the crontab
+
+__author__     = 'Rob Rayborn'
+__copyright__  = 'Copyright 2014, The Mozilla Foundation'
+__license__    = 'MPLv2'
+__maintainer__ = 'Rob Rayborn'
+__email__      = 'rrayborn@mozilla.com'
+__status__     = 'Production'
+
+
 from crontab import CronTab
 import os
 
+#TODO(rrayborn): this might need a csv or even a front end later
 _JOBLIST = [
         #[cron_time_fmt, command, UNIQUE tag that must not be changed]
         #['13 * * * *','echo "this is a new test: $CODE_PATH" >/tmp/test.txt', 'test']
@@ -10,49 +25,91 @@ _JOBLIST = [
             'python pipelines/google_play/google_play_manager.py >/tmp/google_play_cron 2>&1', 
             'Play Pipeline'],
         ['0 4 * * *', 
-            'python pipelines/stats_pipeline/pipeline.py >/tmp/stats_cron 2>&1', 
+            'python pipelines/stats_pipeline/pipeline.py         >/tmp/stats_cron       2>&1', 
             'Stats Pipeline'],
         ['0 5 * * *', 
-            'python pipelines/hello_pipeline/pipeline.py >/tmp/hello_cron 2>&1 && sudo chmod -R 775 /var/server/server', 
+            'python pipelines/hello_pipeline/pipeline.py         >/tmp/hello_cron       2>&1',
             'Hello Pipeline'],
         ['0 */6 * * *',
-            'python pipelines/alert_pipeline/word_alert.py >/tmp/alert_cron 2>&1',
+            'python pipelines/alert_pipeline/word_alert.py       >/tmp/alert_cron       2>&1',
             'Alert Pipeline']
     ]
 
+#TODO(rrayborn): this might need a csv or even a front end later
+_IGNORELIST = [
+            #tags
+        ]
 
-def main(user = None):
+
+def update_cron(user = None):
+    """
+    Updates the user's crontab
+
+    Args:
+        user (string): The word that needs stemming (default = $USER)
+
+    Examples:
+        >>> update_cron("rrayborn")
+    """
     if not user:
         if os.environ['USER']:
             user = os.environ['USER']
         else:
-            raise Exception('User value not populated.')
+            raise Exception('$USER not populated or overridden.')
+    if not os.environ['CODE_PATH']:
+        raise Exception('$CODE_PATH not populated. \
+                        You should use the uabackend virtualenv.')
     
     cron = CronTab(user=user)
 
+    invalid_jobs = []
+
+    # Create/Replace jobs
     for entry in _JOBLIST:
-        time    = entry[0]
-        command = entry[1] \
-            .replace('$CODE_PATH', os.environ['CODE_PATH']) \
-            .replace('python ', os.environ['CODE_PATH'] + \
-                'pipelines/cron/cronwrapper.sh ')
-        tag     = entry[2]
+        time     = entry[0]
+        command  = entry[1] \
+            .replace('python ', '$CODE_PATH/pipelines/cron/python_backend_wrapper.sh ') \
+            .replace('$CODE_PATH', os.environ['CODE_PATH'])
+        tag      = entry[2]
+        auto_tag = 'AUTOGENERATE: ' + tag
+        
+        # find existing jobs
+        existing_jobs = cron.find_comment(auto_tag)
+        is_empty = True
         
         # remove old jobs
-        for job in cron.find_comment(tag):
-            print 'Deleting job: "%s"' % str(job)
+        for job in existing_jobs:
             cron.remove(job)
+            is_empty = False
+
+        # print operation
+        if is_empty:
+            print 'Creating job with tag: "%s"' % str(tag)
+        else:
+            print 'Replacing job(s) with tag: "%s"' % str(tag)
         
         # create new jobs
         job = cron.new(command=command)
         job.setall(time)
-        job.set_comment(tag)
+        job.set_comment(auto_tag)
 
-        # output
-        if not job.is_valid():
-            raise Exception('Job "%s" is not valid' % job)
-        cron.write()
+        # check validity
+        if not job.is_valid(): #TODO(rrayborn): this never seems to be false.
+            invalid_jobs.append(job)
+    
+    # Update ignored jobs
+    for tag in _IGNORELIST:
+        auto_tag = 'AUTOGENERATE: ' + tag
+        for job in cron.find_comment(auto_tag):
+            print 'Ignoring job: "%s"' % str(tag)
+            job.enable(False)
+
+    if invalid_jobs:
+        raise Exception('The following jobs are invalid:\n%s' % \
+                        '\n'.join(invalid_jobs))
+
+    cron.write()
 
 
 if __name__ == '__main__':
-    main(user = 'root')
+    update_cron()
