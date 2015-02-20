9c805f91169554c06340f2a53c6add8900a0fba8
Cheng Wang <cww@mozilla.com>
2015-02-20 15:10:56 -0800

    WIP incorporating alerts API.

--- pipelines/alert_pipeline/word_alert.py
+++ pipelines/alert_pipeline/word_alert.py
@@ -11,6 +11,8 @@ from warnings import warn
 import smtplib
 from email.mime.text import MIMEText
 from sqlalchemy import sql
+from textwrap import dedent
+from math import log
 
 import operator
 
@@ -20,8 +22,20 @@ from lib.language.word_types import tokenize
 
 _TIMEFRAME = 12 # Hours
 _PAST_TIMEFRAME = 3 # Weeks
-_DIFF_PERCENT = 100 # Percentage increase (0 for any increase)
-_DIFF_ABS = 5 # Percentage point change
+# Constants for calculation severity
+_DIFF_PCT_MIN = 10 # Percentage increase for which we absolutely don't want to return sev.
+_DIFF_ABS_MIN = 0.5 # Percentage point change for which we absolutely ignore
+_DIFF_ABS_SCALE = 70 # Scaling factor between rel and abs diff
+_SEV_SCALE = 10 # Factor by which to expand the log of the product of the logs.
+_SEV_SUB = 3.3 # Magic. Just magic. Deal
+
+_EMAIL_SEV_MIN = 5 # Severity level above which we send email
+_ALERT_SEV_MIN = 2 # Severity level above which we send alerts (-1 = send everything)
+
+ALERT_EMAIL_FROM = environ['ALERT_EMAIL_FROM']
+ALERT_EMAIL = environ['ALERT_EMAIL']
+
+ALERT_TOKEN = environ['ALERT_TOKEN']
 
 def main():
     input_db = Db('input')
@@ -87,29 +101,35 @@ def main():
             delta[key].after.insert(key = key, link = row.id, meta = word_set)
         after_total += 1
     
+    #Generate alerts
+    
+    for (k,v) in delta.iteritems():
+        v.set_thresholds(diff_pct = _DIFF_PCT_MIN, diff_abs = _DIFF_ABS_MIN)
+        v.set_potentials(base = base_total, after = after_total)
+        if (v.is_significant and v.severity >= _ALERT_SEV_MIN):
+            emit_alert(v)
+
+    
+    # Now send an email, looking up each piece of feedback.
     email_list = set()
     
     for (k,v) in delta.iteritems():
-        v.set_thresholds(diff_pct = _DIFF_PERCENT, diff_abs = _DIFF_ABS)
-        v.set_potentials(base = base_total, after = after_total)
-        if v.is_significant:
-            print "ALERT FOR %s: Before: %.2f/1000; After %.2f/1000; "\
-                  "Diff: %.2f; %%diff: %.2f" % (
-                ', '.join(v.after.sorted_metadata[0:3]),
-                v.base_pct * 10,
-                v.after_pct * 10,
-                v.diff_abs * 10,
-                v.diff_pct
-            )
+        v.set_thresholds(diff_pct = _DIFF_PCT_MIN, diff_abs = _DIFF_ABS_MIN)
+        if (v.is_significant and v.severity >= _EMAIL_SEV_MIN):
             email_list.add(v)
-    
+    email_results(email_list)
+
+def email_results(email_list):
     email_body = ''
     shortwords = []
     
     rfr = input_db.get_table('remote_feedback_response')
         
     for v in email_list:
-        email_body += "===== ALERT FOR %s: =====\n" % (v.after.sorted_metadata[0])
+        email_body += "===== Lvl %d ALERT FOR %s: =====\n" % (
+            v.severity,
+            v.after.sorted_metadata[0]
+            )
         email_body += "Words: " + ", ".join(v.after.sorted_metadata)
         email_body += "\n\n"
         email_body += "Before: %.2f/1000; After %.2f/1000; Diff: %.2f; %%diff: %.2f"\
@@ -140,21 +160,81 @@ def main():
         shortwords.append(v.after.sorted_metadata[0])
     
     if len(email_list) > 0:
-        email_from = environ['ALERT_EMAIL_FROM']
-        email_to = environ['ALERT_EMAIL']
+
     
         msg = MIMEText(email_body)
         msg['Subject'] = "Alert for: " + ", ".join(shortwords)
-        msg['From'] = email_from
-        msg['To'] = email_to
+        msg['From'] = ALERT_EMAIL_FROM
+        msg['To'] = ALERT_EMAIL
         server = smtplib.SMTP('localhost')
-        server.sendmail(email_from, email_to.split(','), msg.as_string())
+        server.sendmail(ALERT_EMAIL_FROM, ALERT_EMAIL.split(','), msg.as_string())
         server.quit()
     
     print "Run complete. %d alerts issued. %d before and %d after comments processed." % \
         (len(email_list), base_total, after_total)
-    
 
+
+def emit_alert (v):
+    headers = {
+        'content-type': 'application/json',
+        'accept': 'application/json; indent=4',
+        'authorization': 'token '+ALERT_TOKEN,
+    }
+    links = []
+    for link_id in v.after.link_list:
+        links.append('https://input.mozilla.org/dashboard/response/'+link_id)
+    description = dedent("""
+    
+        Trending words: %s
+    
+        Before: %.2f/1000
+        After %.2f/1000
+        Absolute Difference: %.2f %%age points
+        Percent Difference: %.2f %%
+        
+    """%(
+        ", ".join(v.after.sorted_metadata),
+        v.base_pct * 10,
+        v.after_pct * 10,
+        v.diff_abs * 10,
+        v.diff_pct
+    )).strip()
+    payload = {
+        'severity': v.severity,
+        'summary': '%s is trending up by %.2f'%(v.after.sorted_metadata[0], v.diff_pct),
+        'description': description,
+        'flavor': 'word_alert',
+        'emitter_name': 'input_word_alert',
+        'emitter_version': 0,
+        'links': links
+    }  
+    resp = requests.post(
+        'https://input.mozilla.org/api/v1/alerts/alert/',
+        data=json.dumps(payload),
+        headers=headers
+    )
+    
+class WordDeltaCounter (ItemCounterDelta):
+    @property
+    def severity (self):
+        """
+        This function is magic. I don't know if it's the right one or if we should
+        make a better one but whatever.
+        """
+        pct_part = safe_log(self.diff_pct - _DIFF_PCT_MIN)
+        abs_part = safe_log((self.diff_abs - _DIFF_ABS_MIN)*_DIFF_ABS_SCALE)
+        value = _SEV_SCALE * (safe_log(abs_part * pct_part) - _SEV_SUB)
+        if value < -1:
+            return -1
+        if value > 10:
+            return 10
+        return floor(value)
+        
+def safe_log (value):
+    if value <= 0:
+        return 0
+    return log(value)
+    
 if __name__ == '__main__':
 
     main()
