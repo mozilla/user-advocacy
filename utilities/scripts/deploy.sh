9851b87fb925066dca52106bb6879e61f578a48b
Robert Rayborn <rrayborn@mozilla.com>
2015-02-27 10:24:52 -0800

    Fix deploy to require parameter specifying what to deploy

--- utilities/scripts/deploy.sh
+++ utilities/scripts/deploy.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# $1 = 'backend' or 'all' depending on what you want to deploy
 
 # Make sure user is root
 if ( (( EUID != 0 )) || [ ! "$BASH_VERSION" ] ); then
@@ -12,35 +13,41 @@ source $WORKON_HOME/uabackend/bin/virtualenvwrapper.sh
 
 workon uabackend
 
-if [[ -n $CODE_PATH ]]
+if ( [[ -n $CODE_PATH ]] && [[ -n $1 ]] )
 then
-
-    # Check out $CODE_PATH
-    cd $CODE_PATH
-    git pull
-    chgrp -f     -R advocacy ..
-    chmod 771 -f -R          ..
-
-    # Check out /var/server/server
-    cd /var/server/
-    cp -r $CODE_PATH/flask/* server
-    chgrp -f     -R advocacy        server
-    chmod 775 -f -R                 server
-    if [[ -n $UPLOAD_PATH ]]
+    if [ $1 = "backend" ] || [ $1 = "all" ]
     then
-        chown -R www-data:www-data $UPLOAD_PATH
-    else
-        chown -R www-data:www-data server/useradvocacy/reports/uploads
+        # Check out $CODE_PATH
+        cd $CODE_PATH
+        git pull
+        chgrp -f     -R advocacy ..
+        chmod 771 -f -R          ..
+
+        python $CODE_PATH/pipelines/cron/scheduler.py
     fi
 
-    python $CODE_PATH/pipelines/cron/scheduler.py
+    if [ $1 = "all" ]
+    then
+        # Check out /var/server/server
+        cd /var/server/
+        cp -r $CODE_PATH/flask/* server
+        chgrp -f     -R advocacy        server
+        chmod 775 -f -R                 server
+        if [[ -n $UPLOAD_PATH ]]
+        then
+            chown -R www-data:www-data $UPLOAD_PATH
+        else
+            chown -R www-data:www-data server/useradvocacy/reports/uploads
+        fi
+
+        # restart server
+        service apache2 restart
+    fi
 
 else
+    echo "\$CODE_PATH or \$1 not populated"
     exit 1
-
 fi
 
 deactivate
 
-# restart server
-service apache2 restart
