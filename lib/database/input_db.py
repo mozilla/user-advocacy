8b6f49d5329ab328842b5d3c2026cd7e2ca51901
Cheng Wang <cww@mozilla.com>
2015-03-31 12:06:03 -0700

    New DBs.

---
+++ lib/database/input_db.py
@@ -0,0 +1,18 @@
+#!/usr/local/bin/python
+
+"""
+This is the database module for all input tasks. It inherits from db.py for all the
+heavy lifting.
+"""
+
+import os
+from . import db
+
+os_env = os.environ
+
+engine = db.init_engine(os_env['SQLALCHEMY_UTILS_URI'])
+
+class Db(db.Database):
+    def __init__(self, database_name, is_persistent = False):
+        super(Db, self).__init__(database_name, engine, is_persistent)
+    
