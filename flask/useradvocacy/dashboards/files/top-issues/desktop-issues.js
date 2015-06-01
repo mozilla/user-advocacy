9f9007ab1a7ff4a538189ea500f617ee09e035b6
Cheng Wang <cww@mozilla.com>
2015-06-01 12:04:52 -0700

    Refactor issues code to be in charts.js

--- flask/useradvocacy/dashboards/files/top-issues/desktop-issues.js
+++ flask/useradvocacy/dashboards/files/top-issues/desktop-issues.js
@@ -1,13 +1,13 @@
 (function($, d3, window, _) {
 
-
-var desktop = issueTrendLine('desktop_conf.json', "#issue-list", "desktop");
+var desktop = inputTrendLines('desktop_conf.json')
+    .extra_params(['date_delta=180d', 'product=Firefox', 'locale=en-US', 'happy=0']);
 var annoter = makeAnnotations();
 var noteP = d3.jsonPromise("/data/static/json/desktop-annotations.json")
     .done(function(note_data) {
         notes = annoter(note_data);
         desktop.annotations(notes);
-        desktop();
+        d3.select('#issue-list').call(desktop);
     })
 
 }(jQuery, d3, window, _));
