62ac544b821476b0a36f149cf512aacfb9617b7b
Robert Rayborn <rrayborn@mozilla.com>
2015-06-02 14:30:02 -0700

    Fix 2 old references to old dash

--- flask/useradvocacy/dashboards/files/android-top-issues/android-issues.js
+++ flask/useradvocacy/dashboards/files/android-top-issues/android-issues.js
@@ -1,14 +1,14 @@
 (function($, d3, window, _) {
 
-var desktop = inputTrendLines('android_conf.json')
+var android = inputTrendLines('android_conf.json')
     .extra_params(['date_delta=180d', 'products=Firefox for Android', 'locales=en-US', 'happy=0'])
     .link_base('https://input.mozilla.org/en-US/?product=Firefox for Android&locale=en-US&happy=0&');
 var annoter = makeAnnotations();
 var noteP = d3.jsonPromise("/data/static/json/mobile-annotations.json")
     .done(function(note_data) {
         notes = annoter(note_data);
-        desktop.annotations(notes);
-        d3.select('#issue-list').call(desktop);
+        android.annotations(notes);
+        d3.select('#issue-list').call(android);
     })
 
 }(jQuery, d3, window, _));
